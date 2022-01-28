9663c1aa91acd0129f677788896271212bfa2b6e
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-01-28 10:08:39 +0700

    delete server add txt file

--- server/src/route/category.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const CategoryController = require('../controller/category/category.controller')
-
-router.post('/', CategoryController.CreateCategory)
-
-module.exports = router
--- server/src/route/test.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const TestController = require('../controller/test/test.controller')
-
-router.post('/', TestController.CreateTest)
-router.get('/:name', TestController.FindTest)
-
-module.exports = router
