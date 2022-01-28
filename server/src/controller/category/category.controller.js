6017c71a9b6a1e6ab6610adda3dd1c62f2ae5ee0
Spaghetticabonara <61241799+Spaghetticabonara@users.noreply.github.com>
2022-01-28 10:12:33 +0700

    delete on branch test_branch

--- server/src/controller/category/category.controller.js
+++
@@ -1,6 +0,0 @@
-const CategoryService = require('../../service/category.service')
-
-exports.CreateCategory = async (req, res) => {
-    const category = await CategoryService.CreateCategory(req)
-    return res.status(200).json({ data: category })
-}
--- server/src/route/product.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ProductController = require('../controller/product/product.controller')
-
-router.post('/', ProductController.CreateProduct)
-router.get('/:category', ProductController.FindProductByCategory)
-
-module.exports = router
